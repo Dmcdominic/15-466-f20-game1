ef4abbdf521148bf1d9f7b768853585407709399
Dmcdominic <dmcdominic@gmail.com>
2020-09-13 00:35:04 -0400

    import-sprites (asset pipeline) first version implemented. Sprite loading still to be tested

--- import-sprites.cpp
+++ import-sprites.cpp
@@ -7,7 +7,8 @@
 #include <string>
 #include <vector>
 #include <stdint.h>
-#include <fstream> 
+#include <fstream>
+#include <array>
 
 
 //...and for c++ standard library functions:
@@ -43,24 +44,83 @@ int main(int argc, char** argv) {
 // Loads a .png spritesheet at filename into a chunk
 void import_spritesheet(std::string filename, OriginLocation origin) {
 	std::string png_filename = raw_assets_path(filename) + ".png";
-	std::string chunk_filename = dist_assets_path(filename) + ".chunk";
+	std::string chunk_filename = dist_assets_path(filename) + "_CHUNK";
 	// Load the png
 	glm::uvec2 size;
 	std::vector< glm::u8vec4 > data;
 	load_png(png_filename, &size, &data, origin);
-	std::cout << "SIZE:\n" << (size.x) << ", " << (size.y) << "\n";
-	// TODO - Iterate over the png data, converting it to tiles and a palette
-	//std::array<PPU466::Palette, 8U> palette_table;
-	auto data_iter = data.begin();
-	for (uint16_t r = 0; r < size.x; r++) {
-		for (uint16_t c = 0; c < size.y; c++) {
-			assert(data_iter != data.end());
-			//TODO
+	std::cout << "SIZE: " << (size.x) << ", " << (size.y) << "\n";
+	
+	// Iterate over the png data, converting it to tiles and a palette
+	SpriteSet s;
+	uint8_t colorTotal = 0; // Tracks how many new colors we've found and placed in palette
 
-			data_iter++;
+	std::cout << "about to enter the big for loops" << std::endl;
+	for (uint16_t sprite_x = 0; glm::u32(sprite_x * (SPRITE_W + SPRITE_PAD) + SPRITE_W) <= size.x; sprite_x++) {
+		for (uint16_t sprite_y = 0; glm::u32(sprite_y * (SPRITE_H + SPRITE_PAD) + SPRITE_H) <= size.y; sprite_y++) {
+			// Now iterate over the 64 pixels in this 8x8 sprite
+			std::cout << "sprite_x: " << sprite_x << ", sprite_y: " << sprite_y << std::endl;
+			PPU466::Tile tile = PPU466::Tile();
+			uint8_t sx = 0; //the x index into the 8x8 tile
+			uint8_t sy = 0;
+			for (uint16_t x = sprite_x * (SPRITE_W + SPRITE_PAD); x < sprite_x * (SPRITE_W + SPRITE_PAD) + SPRITE_W; x++) {
+				sy = 0;
+				for (uint16_t y = sprite_y * (SPRITE_H + SPRITE_PAD); y < sprite_y * (SPRITE_H + SPRITE_PAD) + SPRITE_H; y++) {
+					uint16_t i = x + y * size.x;
+					assert(i < size.x * size.y);
+
+					uint8_t colr;
+					for (colr = 0; colr < colorTotal && colr < s.palette.size(); colr++) {
+						if (s.palette[colr].r == data.at(i).r && s.palette[colr].g == data.at(i).g && s.palette[colr].b == data.at(i).b && s.palette[colr].a == data.at(i).a) {
+							break;
+						}
+					}
+
+					if (colr == colorTotal) {
+						if (colorTotal >= s.palette.size()) {
+							std::cerr << "Exceeded " << s.palette.size() << " colors in spritesheet: " << filename << std::endl;
+							throw;
+						}
+						s.palette[colr].r = data.at(i).r;
+						s.palette[colr].g = data.at(i).g;
+						s.palette[colr].b = data.at(i).b;
+						s.palette[colr].a = data.at(i).a;
+						colorTotal++;
+					}
+					// Set bit0 and bit1 for the tile
+					if (colr % 2 == 1) {
+						tile.bit0[sy] += 1 << (SPRITE_W - sx - 1);
+					}
+					if (colr > 1) {
+						tile.bit1[sy] += 1 << (SPRITE_W - sx - 1);
+					}
+					sy++;
+				}
+				sx++;
+			}
+			assert(sx == SPRITE_W && sy == SPRITE_H);
+			s.tiles.emplace_back(tile);
 		}
 	}
-	assert(data_iter == data.end());
+	std::cout << "past the big for loops" << std::endl;
+
+	// Format the palette and tile together into one chunk
+	data.clear();
+	for (uint8_t colr = 0; colr < s.palette.size(); colr++) {
+		data.emplace_back(s.palette[colr]);
+	}
+	glm::u8vec4 vec;
+	for (auto t_iter = s.tiles.begin(); t_iter != s.tiles.end(); t_iter++) {
+		vec = glm::u8vec4(t_iter->bit0[0], t_iter->bit0[1], t_iter->bit0[2], t_iter->bit0[3]);
+		data.emplace_back(vec);
+		vec = glm::u8vec4(t_iter->bit0[4], t_iter->bit0[5], t_iter->bit0[6], t_iter->bit0[7]);
+		data.emplace_back(vec);
+		vec = glm::u8vec4(t_iter->bit1[0], t_iter->bit1[1], t_iter->bit1[2], t_iter->bit1[3]);
+		data.emplace_back(vec);
+		vec = glm::u8vec4(t_iter->bit1[4], t_iter->bit1[5], t_iter->bit1[6], t_iter->bit1[7]);
+		data.emplace_back(vec);
+	}
+
 	// Save the tile and palette into one chunk
 	std::ofstream chunk_file(chunk_filename, std::ios::binary);
 	write_chunk(std::string("sprt"), data, &chunk_file);
--- import-sprites.hpp
+++ import-sprites.hpp
@@ -11,10 +11,29 @@
 #include <string>
 #include <vector>
 #include <stdint.h>
+#include <array>
+#include "PPU466.hpp"
 
 
 const std::string RAW_ASSETS_DIR_NAME = "raw_assets";
 const std::string DIST_ASSETS_DIR_NAME = "dist/assets";
 
+const uint8_t SPRITE_W = 8;
+const uint8_t SPRITE_H = 8;
+const uint8_t SPRITE_PAD = 2;
+
+
+struct SpriteSet {
+  std::array< glm::u8vec4, 4 > palette = {
+		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
+	};
+  std::vector< PPU466::Tile > tiles = std::vector< PPU466::Tile >();
+};
+
+
 void import_spritesheet(std::string filename, OriginLocation origin);
 std::string raw_assets_path(std::string const& suffix);
+std::string dist_assets_path(std::string const& suffix);
